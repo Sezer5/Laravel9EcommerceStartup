82b3f36ea97f93ffe40d7974bc213b9c00a519d2
Sezer5 <unalmissezer@gmail.com>
2022-11-03 17:11:51 +0300

    First Controller With Laravel

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    //
+    public function index(){
+        echo "Index Function";
+    }
+    public function test(){
+        return view('home.test');
+    }
+    public function param($id,$number){
+        
+        echo "Parameter 1:",$id;
+        echo "Parameter 2:",$number;
+        echo "Parameter 2:",$number + $id;
+        return view('home.test2',
+                    [
+            'id' => $id,
+            'number' => $number,
+            
+            
+        ]);
+        
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// 1- do something in route
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+// 2- Call something route
+
+Route::get('/', [\App\Http\Controllers\HomeController::class,'index'])->name('home');
+
+// 3- Call controller function
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
+// 4- Route->Controller->View
+
+Route::get('/test', [HomeController::class,'test'])->name('test');
+
+// 5- Route with parameter
+
+Route::get('/param/{id}', [HomeController::class,'param'])->name('param');
+
+// 5- Route with parameter
+
+Route::get('/param/{id}/{number}', [HomeController::class,'param'])->name('param');
+
 
 Route::middleware([
     'auth:sanctum',
