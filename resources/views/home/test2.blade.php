3c9fe97ca99862d3fb58f4daf915d23eb64fcaf5
Sezer5 <unalmissezer@gmail.com>
2022-11-04 11:30:46 +0300

    Some Changes with value or something

---
+++ resources/views/home/test2.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <h1>First Laravel Page</h1>
+    Id value is {{$id}} <br>
+    Id value is {{$number}} <br>
+</body>
+</html>
